67ffc73bb644209c17f536dd34b6224d124e0185
Bkhutton <bkhutton1@gmail.com>
2019-04-16 20:59:40 -0400

    Changed structure

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, Response, render_template
+from flask import Flask
+from flask import Response
+from flask import request
+from flask import render_template
 import logging as log
 
 app = Flask(__name__)
 
-def standard_page(html_file, page_title):
+def standard_page(html_file,title):
     content = ''
 
-    content = content + render_template('header.html', page_title=page_title)
+    content = content + render_template('header.html', title=title)
 
     with open(html_file) as index:
         content = content + index.read()
@@ -17,12 +20,25 @@ def standard_page(html_file, page_title):
 
 @app.route('/')
 def index():
-    content = standard_page('index.html', 'Home')
+    content = standard_page('index.html', "Home")
     return Response(content, mimetype='text/html')
 
-@app.route('/generate.html')
+@app.route('/generate.html', methods=['POST', 'GET'])
 def generate():
-    content = standard_page('generate.html', 'Generate')
+    if 'POST' == request.method:
+        raw_input = request.form['input_text']
+        results = raw_input
+    else:
+        results = 'Results will display here!'
+
+    content = ''
+
+    content = content + render_template('header.html', title='Generate')
+
+    content = content + render_template('generate.html', results=results)
+    
+    content = content + render_template('footer.html')
+
     return Response(content, mimetype='text/html')
 
 @app.route('/about.html')
